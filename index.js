526ae3b5f84c60e6d001afe31bf218cc190b8ae5
Carlos Lezcano <carlos.lezcano988@gmail.com>
2020-12-21 09:31:38 -0600

    SPA control

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 //leyendo variables de entrono 
 require('dotenv').config();
 //
+const path =require('path');
 //cors
 const cors = require('cors');
 //
@@ -31,6 +32,13 @@ app.use('/api/medicos'   ,    require('./routes/Routes-medicos'   ) );
 app.use('/api/todo'      ,    require('./routes/Routes-busquedas' ) );
 app.use('/api/login'     ,    require('./routes/Routes-auth'      ) );
 app.use('/api/uploads'   ,    require('./routes/Routes-uploads'   ) );
+
+// lo ultimo ya cuando he desplegado mi apliacion para que todas mis rutas pasen por el idex y si hacen 
+// un resfresh de la pagina no ocurra ningun error.
+ app.get('*',(req,res) =>{
+  res.sendFile(path.resolve(__dirname,'public/index.html'));
+
+ });
 
 // para levantarlo
 app.listen( process.env.PORT, () => {
